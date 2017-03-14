e9a5b7d412350e845e57f04697d3fab7f8e94489
pasaunders <kaorti@gmail.com>
2017-03-13 19:56:54 -0700

    home view now returns minimal html, passes unit tests.

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.http import HttpRequest
 from django.core.urlresolvers import resolve
 from lists.views import home_page
 
@@ -10,3 +11,12 @@ class HomePageTest(TestCase):
         """Unit test, checks whether the home page is at the expected url."""
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_html(self):
+        """Unit test checking that home page html is as expected."""
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
