0d5a72bd74b83de528dbadcbe11fdc25e9e0174b
pasaunders <kaorti@gmail.com>
2017-04-18 22:38:18 -0700

    refactored functional tests with helper function to test for list entries.

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -3,6 +3,7 @@ from django.http import HttpRequest
 from django.core.urlresolvers import resolve
 from django.template.loader import render_to_string
 from lists.views import home_page
+from lists.models import Item
 
 
 class HomePageTest(TestCase):
@@ -23,3 +24,10 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
