12b60b9adaa2ae4a8c3bc9d337fa70afc256b6ba
pasaunders <kaorti@gmail.com>
2017-04-20 22:09:27 -0700

    removed corpse code, saving post requests and redirecting

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -22,8 +22,17 @@ class HomePageTest(TestCase):
     def test_save_POST(self):
         """Unit test checking that home page can save a post request."""
         response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
 
 
 class ItemModelTest(TestCase):
@@ -43,4 +52,4 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(second_saved_item, 'Item the second')
+        self.assertEqual(second_saved_item.text, 'Item the second')
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from lists.models import Item
 
 
 def home_page(request):
-    # if request.method == 'POST':
-    #     return HttpResponse(request.POST['item_text'])
-    return render(request, 'home.html', {
-            'new_item_text': request.POST.get('item_text', '')
-        })
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
+    return render(request, 'home.html')
